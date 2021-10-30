f70360f46373cdba95e5cc4a94463b313bb339de
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2021-10-31 00:21:30 +0300

    c4

--- js/index.js
+++ js/index.js
@@ -1,22 +1,10 @@
-function wrapInParagraph(str) {
-    let arr = str.split('\n')
-    let sum = ''
-    for (i = 0; i < arr.length; i++) {
-        sum += `<p>${arr[i]}</p>\n`
-    }
+let str = 'elEVen'
+console.log(str);
+let txt = str.toLowerCase()
+console.log(txt);
+let arr = {e: 0};
+console.log(arr);
+let keys = {e};
+console.log(keys);
 
-    console.log(sum.substr(0, sum.length - 3));
-
-    return sum
-}
-
-const text = `Some
-simple multiline
-text`;
-
-console.log(wrapInParagraph(text));
-
-const text2 = 'some\ntext';
-
-console.log(wrapInParagraph(text2)); // <p>some</p>
-// <p>text</p>
+console.log(txt[2]);
